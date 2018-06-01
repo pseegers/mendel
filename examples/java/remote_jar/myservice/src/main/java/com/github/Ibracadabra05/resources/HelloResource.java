29b0ff8b5156e3c22c2b7fc8784380e065645d11
Ibrahim Hamisu <ibrahim@sproutsocial.com>
2018-06-01 10:42:04 -0500

    Add resource class for remote_jar example service

---
+++ examples/java/remote_jar/myservice/src/main/java/com/github/Ibracadabra05/resources/HelloResource.java
@@ -0,0 +1,18 @@
+package com.github.Ibracadabra05.resources;
+
+import javax.validation.Valid;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Produces(MediaType.TEXT_PLAIN)
+@Path(("/hello"))
+public class HelloResource {
+
+    @GET
+    public String getHello(@Valid @QueryParam("name") String name) {
+        return "Hello, " + name + "!\n";
+    }
+}
